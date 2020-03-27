d72f6633445dea5949cc6b0cdd668cee71605ea7
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2020-03-26 21:42:08 -0300

    add error class

---
+++ src/main/java/com/fcamara/backendtestjava/errors/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fcamara.backendtestjava.errors;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	public ResourceNotFoundException(String message) {
+		 super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
