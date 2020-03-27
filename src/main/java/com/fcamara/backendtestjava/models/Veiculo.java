7d24a6d740dfdea397bbe088fdb341c848b0ee96
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2020-03-27 18:23:04 -0300

    update model veiculo

--- src/main/java/com/fcamara/backendtestjava/models/Veiculo.java
+++ src/main/java/com/fcamara/backendtestjava/models/Veiculo.java
@@ -1,5 +1,81 @@
 package com.fcamara.backendtestjava.models;
 
-public class Veiculo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
+@Entity
+@Table(name = "veiculo")
+public class Veiculo {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO) // gerando um codigo automaticante do tipo long
+	private long codigo;
+	
+	@NotEmpty
+	private String marca;
+	
+	private String modelo;
+	
+	@NotEmpty
+	private String cor;
+	
+	@NotEmpty
+	private String placa;
+	
+	@NotEmpty
+	private String tipo;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
